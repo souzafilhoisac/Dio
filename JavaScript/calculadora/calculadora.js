2a6634e17cdcb7c4b8181b8e308dd91a940341a0
souzafilhoisac <souzafilhoisac@gmail.com>
2022-05-27 19:56:03 -0300

    Exercício calculadora

---
+++ JavaScript/calculadora/calculadora.js
@@ -0,0 +1,99 @@
+function calculadora() {
+    const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - multiplicação (*)\n 4 - Divisão real(/)\n 5 - Divisão inteira (%)\n 6 - Potenciação (**)'))
+    
+    if (!operacao || operacao >= 7) {
+        alert('Operação inválida!')
+        calculadora();
+    } else {
+        let n1 = Number(prompt('Insira o primeiro valor:'));
+        let n2 = Number(prompt('Insira o segundo valor:'));
+        let res;
+
+        if (!n1 || !n2) {
+            alert('[ERRO] - Parâmetros inválidos');
+            calculadora();
+        } else {
+            function soma() {
+                res = n1 + n2;
+                alert(`${n1} + ${n2} = ${res}`);
+                novaOperacao();
+            }
+            function subtracao() {
+                res = n1 - n2;
+                alert(`${n1} - ${n2} = ${res}`);
+                novaOperacao();
+            }
+            function multuplicacao() {
+                res = n1 * n2;
+                alert(`${n1} x ${n2} = ${res}`);
+                novaOperacao();
+            }
+            function divisaoReal() {
+                res = n1 / n2;
+                alert(`${n1} / ${n2} = ${res}`);
+                novaOperacao();
+            }
+            function divisaoInteira() {
+                res = n1 % n2;
+                alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${res}`);
+                novaOperacao();
+            }
+            function potenciacao() {
+                res = n1 ** n2;
+                alert(`${n1} elevado a ${n2} é igual a ${res}`);
+                novaOperacao();
+            }
+    
+            function novaOperacao() {
+                let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não')
+                if (opcao == 1) {
+                    calculadora();
+                } else if (opcao == 2) {
+                    alert('Até mais!');
+                } else {
+                    alert('Digite uma opção válida!')
+                    novaOperacao();
+                }
+            }
+        }
+
+        
+
+        // if (operacao == 1) {
+        //     soma();
+        // } else if(operacao == 2) {
+        //     subtracao();
+        // } else if(operacao == 3) {
+        //     multuplicacao();
+        // } else if(operacao == 4) {
+        //     divisaoReal();
+        // } else if(operacao == 5) {
+        //     divisaoInteira();
+        // } else if(operacao == 6) {
+        //     potenciacao();
+        // }
+        switch (operacao) {
+            case 1:
+                soma();
+                break;
+            case 2:
+                subtracao();
+                break;
+            case 3:
+                multuplicacao();
+                break;
+            case 4:
+                divisaoReal();
+                break;
+            case 5:
+                divisaoInteira();
+                break;
+            case 6:
+                potenciacao();
+                break;
+        }
+            
+    }
+}
+
+calculadora()
