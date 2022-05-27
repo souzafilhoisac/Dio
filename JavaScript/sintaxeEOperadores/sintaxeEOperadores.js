2390e35d6ab7514097c3236348bbd9a567f184fd
souzafilhoisac <souzafilhoisac@gmail.com>
2022-05-27 20:40:47 -0300

    Exercício com if ternario

---
+++ JavaScript/sintaxeEOperadores/sintaxeEOperadores.js
@@ -0,0 +1,9 @@
+function numeros(num1,num2) {
+    let comparacao = num1 === num2 ? `são iguais` : `não são iguais`;
+    let soma = num1 + num2;
+    let maiorQue10 = soma > 10 ? `maior que 10` : `menor que 10`;
+    let menorQue20 = soma < 20 ? `menor que 20` : `maior que 20`;
+    return `Os números ${num1} e ${num2} ${comparacao}. Sua soma é ${soma}, que é ${maiorQue10} e ${menorQue20}.`;
+}
+
+console.log(numeros(16, 16))
